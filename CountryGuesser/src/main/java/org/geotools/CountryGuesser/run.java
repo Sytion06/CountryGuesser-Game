f41516b2e0a1dafc83e463defb210baacec27377
Sytion06 <133545799+Sytion06@users.noreply.github.com>
2025-01-10 16:56:22 -0500

    Delete CountryGuesser directory

--- CountryGuesser/src/main/java/org/geotools/CountryGuesser/run.java
+++
@@ -1,9 +0,0 @@
-package org.geotools.CountryGuesser;
-
-
-public class run {
-    public static void main(String[] args) {
-        String shapefilePath = "\\Users\\18261\\CountryGuesser\\target\\generated-sources\\ne_50m_admin_0_countries.shp";
-        new CountryGuess(shapefilePath);
-    }
-}
